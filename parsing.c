0e806497d08b5e06815c0666e0121af098c6f719
guzmanga <guzmanga@oregonstate.edu>
2017-12-31 22:21:33 -0800

    changed grammar to recognize operators in text format (add, sub, mul, div)

--- parsing.c
+++ parsing.c
@@ -48,7 +48,7 @@ int main(int argc, char** arvg){
 	mpca_lang(MPCA_LANG_DEFAULT,
 		"					\
 		number: /-?[0-9]+/;		\
-		operator: '+'|'-'|'*'|'/'|'%'|;	\
+		operator: '+'|'-'|'*'|'/'|'%'|/(add)|(sub)|(mul)|(div)/;	\
 		expr : <number> | '(' <operator><expr>+')';\
 		lispy: /^/<operator><expr>+/$/;	\
 		",
